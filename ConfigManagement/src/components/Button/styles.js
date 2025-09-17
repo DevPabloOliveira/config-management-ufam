fc94451ce4ec78b93f718e3e205b1d3e34f45461
Chrisrodrigues604 <chrisrodrigues604@gmail.com>
2025-09-17 11:36:55 -0300

    estrutura do repositório inciada

---
+++ ConfigManagement/src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const MyButton = styled.button `
+    color: white;
+    background-color: black;
+    border: 1px solid white;
+    width: auto;
+    height: 2em;
+    cursor: pointer;
+    border-radius: 3px;
+`
