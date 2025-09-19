bc443696bd023383510acb2d43119e81dd55dda3
Will <william.tavaresct17@gmail.com>
2025-09-18 20:12:35 -0400

    feat: adiciona div simples de login no componente Button

--- ConfigManagement/src/components/Button/index.jsx
+++ ConfigManagement/src/components/Button/index.jsx
@@ -4,6 +4,14 @@ function Button(){
     return (
         <div>
             <MyButton>Clique</MyButton>
+
+            {/* Nova div para login */}
+            <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc' }}>
+                <h3>Login</h3>
+                <input type="text" placeholder="Usuário" style={{ display: 'block', marginBottom: '10px' }} />
+                <input type="password" placeholder="Senha" style={{ display: 'block', marginBottom: '10px' }} />
+                <MyButton>Entrar</MyButton>
+            </div>
         </div>
     )
 }
